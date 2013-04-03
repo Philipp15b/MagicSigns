2d37e90bad886b819a9feba7bf0ff9f93a5f5311
Philipp Schroer <hebipp1@googlemail.com>
2013-04-03 11:05:40 +0200

    Reformat MSMsg

--- src/main/java/de/philworld/bukkit/magicsigns/util/MSMsg.java
+++ src/main/java/de/philworld/bukkit/magicsigns/util/MSMsg.java
@@ -5,19 +5,19 @@ import org.bukkit.command.CommandSender;
 
 public enum MSMsg {
 
-	NO_PERMISSION(ChatColor.RED + "You don't have permission to do this."), SIGN_CREATED(
-			ChatColor.GREEN + "Your new Magic Sign has been created!"), HEAL_SUCCESS(
-			ChatColor.GREEN
-					+ "You have been healed. Do you already feel refreshed?"), ROCKETED(
-			ChatColor.GREEN + "Whoosh!"), FEED_SUCCESS(ChatColor.GREEN
-			+ "Nom nom nom nom..."), LEVEL_ADDED(ChatColor.GREEN
-			+ "You have been taught % levels."), TELEPORT_SUCCESS(
-			ChatColor.GREEN + "You have been teleported."), INVENTORY_CLEARED(
-			ChatColor.GREEN + "Your inventory has been cleared."), NOT_ENOUGH_MONEY(
-			"You can't pay this sign. Make sure you have %."), PAID_SIGN(
-			"You paid % to use this sign."), POINT_AT_SIGN(ChatColor.RED
-			+ "Please point at a sign!"), NOT_MAGIC_SIGN(ChatColor.GREEN
-			+ "This is just a normal sign!");
+	NO_PERMISSION(ChatColor.RED + "You don't have permission to do this."),
+	SIGN_CREATED(ChatColor.GREEN + "Your new Magic Sign has been created!"),
+	HEAL_SUCCESS(ChatColor.GREEN
+			+ "You have been healed. Do you already feel refreshed?"),
+	ROCKETED(ChatColor.GREEN + "Whoosh!"),
+	FEED_SUCCESS(ChatColor.GREEN + "Nom nom nom nom..."),
+	LEVEL_ADDED(ChatColor.GREEN + "You have been taught % levels."),
+	TELEPORT_SUCCESS(ChatColor.GREEN + "You have been teleported."),
+	INVENTORY_CLEARED(ChatColor.GREEN + "Your inventory has been cleared."),
+	NOT_ENOUGH_MONEY("You can't pay this sign. Make sure you have %."),
+	PAID_SIGN("You paid % to use this sign."),
+	POINT_AT_SIGN(ChatColor.RED + "Please point at a sign!"),
+	NOT_MAGIC_SIGN(ChatColor.GREEN + "This is just a normal sign!");
 
 	private String msg;
 
