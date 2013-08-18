Philipp15b/MagicSigns

commit 4530f0e73962500c7a67e780ac56cfe1442ce818

Fix VaultEconomy#isFree()

src/main/java/de/philworld/bukkit/magicsigns/economy/Price.java
@@ -94,7 +94,7 @@ public abstract class Price {
 
 		@Override
 		public boolean isFree() {
-			return price == 0;
+			return MagicSigns.getEconomy() == null || price == 0;
 		}
 
 		@Override

commit 98efabb9f597293376bd53a21fbe06f99fcbabf6

Apparently 4530f0e broke the test

src/test/java/de/philworld/bukkit/magicsigns/economy/PriceTest.java
@@ -4,12 +4,29 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
 
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.powermock.reflect.Whitebox;
+
+import de.philworld.bukkit.magicsigns.MagicSigns;
+import net.milkbowl.vault.economy.Economy;
 
 public class PriceTest {
+
+	@BeforeClass
+	public static void beforeClass() {
+		Whitebox.setInternalState(MagicSigns.class, Economy.class, mock(Economy.class));
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		Whitebox.setInternalState(MagicSigns.class, Economy.class, (Object) null);
+	}
 
 	@Test
 	public void testVaultEconomyPrice() {